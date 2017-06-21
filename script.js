111f6531b8e16ff0fda0d4876b731226778dcaf0
Edwin Chen <edwinchen85@gmail.com>
2017-06-21 23:20:59 +0800

    Form validation

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function submitAnswers() {
   var q3 = document.forms["quizForm"]["q3"].value;
   var q4 = document.forms["quizForm"]["q4"].value;
   var q5 = document.forms["quizForm"]["q5"].value;
+
+  // Validation
+  for ( i = 1; i <= total; i++ ) {
+    if ( eval('q' + i) == null || eval('q' + i) == '' ) {
+      alert( 'You missed question' + i );
+      return false;
+    }
+  }
 }
